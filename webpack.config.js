a0aa794a889fe4fb5528f929484902018dce4a9e
szenga-pang <accounts@pangszenga.com>
2023-01-09 16:06:16 +0000

    Add comments + breakout babel + set mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,53 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)x?$/,
-        exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
-        loader: "babel-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Custom template",
-      // Load a custom template (lodash by default)
-      template: "./src/index.html",
-    }),
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
+module.exports = (env) => {
+  return {
+    mode: env.mode || "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
     },
-    compress: true,
-    port: 9000,
-  },
+    module: {
+      /**
+       * Array of rules:
+       * Matches / handles requests when a modules is created when compiling
+       * Rules can apply loaders to the modules or modify the parser
+       */
+      rules: [
+        {
+          test: /\.(js)x?$/,
+          exclude: /node_modules/,
+          /**
+           * babel-loader
+           * Input js / jsx files --> browser understandable file
+           * Automatically picks up babel config files
+           */
+          loader: "babel-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"], // Handles imports where extensions are not explicitly stated
+    },
+    plugins: [
+      /**
+       * HtmlWebpackPlugin links custom index.html to bundle.js
+       * inserts the named build file in script tags automatically in index.html
+       */
+      new HtmlWebpackPlugin({
+        title: "Custom template",
+        // Load a custom template
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "build"),
+      },
+      compress: true,
+      port: 9000,
+    },
+  };
 };
