3426d4afd0913892ab336775ef4519f2ab3e986f
Sze Nga Pang <accounts@pangszenga.com>
2023-04-20 14:42:37 +0100

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = (env) => ({
   mode: env.mode || 'development',
@@ -8,7 +8,7 @@ module.exports = (env) => ({
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[contenthash].bundle.js',
     publicPath: '/',
-    clean: true, // Clean build folder before making new bundles
+    clean: true // Clean build folder before making new bundles
   },
   module: {
     /**
@@ -25,13 +25,14 @@ module.exports = (env) => ({
            * Input js / jsx files --> browser understandable file
            * Automatically picks up babel config files
            */
-        loader: 'babel-loader',
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Handles imports where extensions are not explicitly stated
+    extensions: ['.js', '.jsx', '.ts', '.tsx'] // Handles imports where extensions are not explicitly stated
   },
+  devtool: 'source-map',
   plugins: [
     /**
        * HtmlWebpackPlugin links custom index.html to bundle.js
@@ -40,16 +41,16 @@ module.exports = (env) => ({
     new HtmlWebpackPlugin({
       title: 'Custom template',
       // Load a custom template
-      template: './src/index.html',
-    }),
+      template: './src/index.html'
+    })
   ],
   devServer: {
     // This is a SPA, we do not want the application to look for /* and instead for index.html
     historyApiFallback: true,
     static: {
-      directory: path.join(__dirname, 'build'),
+      directory: path.join(__dirname, 'build')
     },
     compress: true,
-    port: 9000,
-  },
-});
+    port: 9000
+  }
+})
