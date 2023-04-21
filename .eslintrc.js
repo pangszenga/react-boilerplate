7d7f6842dd165f47bfa622da4b3b8179f013e451
Sze Nga Pang <accounts@pangszenga.com>
2023-04-21 17:21:29 +0100

    Clean comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,14 +25,17 @@ module.exports = {
     'react'
   ],
   rules: {
-    // This ensures that the 'preferred style' is used for react imports:
-    // "
-    //   Change all default React imports (i.e. import React from "react") to destructured
-    //   named imports (ex. import { useState } from "react") which is the preferred style
-    //   going into the future.
-    // "
-    // There is a codemod here created by the react team to change this in your codebase:
-    // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports
+    /**
+     * This ensures that the 'preferred style' is used for react imports:
+     * "
+     *   Change all default React imports (i.e. import React from "react") to destructured
+     *   named imports (ex. import { useState } from "react") which is the preferred style
+     *   going into the future.
+     * "
+     * There is a codemod here created by the react team to change this in your codebase:
+     * https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports
+    */
+
     'no-restricted-imports': [
       'error',
       {
